62d0e1067201699e44c80d9f5f197003f1233235
xingqiyi <448378892@qq.com>
2017-08-25 17:59:29 +0800

    antd

--- RN/lh/index.android.js
+++ RN/lh/index.android.js
@@ -4,50 +4,25 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
 
-export default class lh extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+import React from 'react';
+import {
+    AppRegistry,
+    Text,
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome',
+    };
+    render() {
+        return <Text>Hello, Navigation!</Text>;
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const SimpleApp = StackNavigator({
+    Home: { screen: HomeScreen },
 });
 
-AppRegistry.registerComponent('lh', () => lh);
+AppRegistry.registerComponent('lh', () => SimpleApp);
---
+++ RN/lh/src/MainScreen.js
@@ -0,0 +1,16 @@
+class MainScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome',
+    };
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <Button
+                title="Go to Jane's profile"
+                onPress={() =>
+                    navigate('Profile', { name: 'Jane' })
+                }
+            />
+        );
+    }
+}
