e74d707aa03c7c9725ab47a56f405aca4cb05f44
xingqiyi <448378892@qq.com>
2017-08-25 18:09:46 +0800

    navigation

--- RN/lh/index.android.js
+++ RN/lh/index.android.js
@@ -8,7 +8,9 @@
 import React from 'react';
 import {
     AppRegistry,
+    View,
     Text,
+    Button,
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
@@ -17,12 +19,46 @@ class HomeScreen extends React.Component {
         title: 'Welcome',
     };
     render() {
-        return <Text>Hello, Navigation!</Text>;
+        const { navigate } = this.props.navigation;
+
+        return (
+            <View>
+                <Text>Hello, Chat App!</Text>
+                <Button
+                    onPress={() => navigate('Chat')}
+                    title="Chat with Lucy"
+                />
+            </View>
+        );
     }
 }
 
+
+
+
+
+class ChatScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Chat with Lucy',
+    };
+    render() {
+        return (
+            <View>
+                <Text>Chat with Lucy</Text>
+            </View>
+        );
+    }
+}
+
+
+
+
+
 const SimpleApp = StackNavigator({
     Home: { screen: HomeScreen },
+    Chat: { screen: ChatScreen },
 });
+
+
 
 AppRegistry.registerComponent('lh', () => SimpleApp);
