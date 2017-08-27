e31daebde35a7bd0075fe5791b142ad909397b2a
sxc <shuaissss@126.com>
2017-08-27 20:37:41 +0800

    lh

---
+++ RN/lh/main.js
@@ -0,0 +1,19 @@
+// import React, { Component } from 'react';
+// import { AppRegistry } from 'react-native';
+
+// import HomePage from './src/Root';
+
+// class lh extends Component {
+//     render() {
+//         return (
+//             <HomePage></HomePage>
+//         );
+//     }
+// }
+
+// AppRegistry.registerComponent('lh', () => lh);
+
+import { AppRegistry } from "react-native";
+import SimpleApp from "./src/app";
+
+AppRegistry.registerComponent("lh", () => SimpleApp);
