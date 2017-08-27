c213aa5c768587530e575d13cc5d78583d61cd6c
sxc <shuaissss@126.com>
2017-08-27 11:47:23 +0800

    clean

--- RN/ant-design-mobile-master/index.js
+++
@@ -1,9 +0,0 @@
-// Just import style for https://github.com/ant-design/ant-design/issues/3745
-const req = require.context('./components', true, /^\.\/[^_][\w-]+\/style\/index\.tsx?$/);
-
-// build for `dist/antd-mobile.css`
-req.keys().forEach((mod) => {
-  req(mod);
-});
-
-module.exports = require('./components/index');
